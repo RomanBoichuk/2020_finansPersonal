4f5e16a768c0e92d1b2f3a8f4ec243ff05e6a515
Roman <you@example.com>
2020-11-24 16:52:57 +0200

    init body-parser render profitFile

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require('express');
+var bodyParser = require('body-parser')
 const app = express()
 const documentController = require('./controllers/document-controller');
 const fs = require('fs');
+
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 
 app.set('view engine', 'pug');
 
 
 app.get("/", documentController.maintable)
-app.get("/profit-table",documentController.profittable)
-app.get("/expense-table",documentController.expensetable)
+app.get("/profit-table", documentController.profittable)
+app.get("/expense-table", documentController.expensetable)
+app.post("/profit-table", documentController.createProfit)
 
 
 
--- controllers/document-controller.js
+++ controllers/document-controller.js
@@ -1,3 +1,4 @@
+var moment = require('moment');
 const fs = require('fs');
 
 
@@ -11,4 +12,21 @@ exports.profittable = function (req, res) {
 
 exports.expensetable = function (req, res) {
   res.render("expense-table")
+}
+
+exports.createProfit = function (req, res){
+  var fileProfitName = moment().format("DD-MM-YYYY")+".json"
+  var fileProfit = []
+  try {
+    if (fs.existsSync(fileProfitName)) {
+      fileProfit=fs.readFileSync(fileProfitName, 'utf-8')
+      fileProfit=JSON.parse(fileProfit)
+    }
+  } catch(err) {
+    console.error(err)
+  }
+  fileProfit.push(req.body)
+  fs.writeFileSync(fileProfitName, JSON.stringify(fileProfit))
+
+  res.redirect("/")
 }
